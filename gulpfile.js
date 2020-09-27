02f92e1cec466f009e1dd2a46385e326b4684391
Teo Winkler <teo.winkler@gmail.com>
2020-09-27 20:34:49 +0200

    fix watch gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -1,34 +1,19 @@
-var gulp = require('gulp')
-var browserSync = require('browser-sync').create()
-var sass = require('gulp-sass')
-var rename = require('gulp-rename')
-var useref = require('gulp-useref')
-var cssnano = require('gulp-cssnano')
+const gulp = require('gulp')
+const browserSync = require('browser-sync').create()
+const sass = require('gulp-sass')
+const rename = require('gulp-rename')
+const useref = require('gulp-useref')
+const cssnano = require('gulp-cssnano')
 
 /* Tasks */
 
-gulp.task('browserSync', function () {
-  browserSync.init({
-    server: {
-      baseDir: 'app'
-    },
-    files: ['css/**/*.css'],
-    browser: 'default',
-    injectChanges: true,
-    watchTask: true,
-    notify: false
-  })
-})
-
 gulp.task('sass', function () {
-  return gulp.src('app/scss/colored-pages.scss')
-    .pipe(sass())
-    .pipe(gulp.dest('app/css'))
+  return gulp.src('./app/scss/colored-pages.scss').pipe(sass()).pipe(gulp.dest('./app/css')).pipe(browserSync.stream())
 })
 
 gulp.task('dist:css', function () {
   return gulp
-    .src('app/scss/colored-pages.scss')
+    .src('./app/scss/colored-pages.scss')
     .pipe(sass())
     .pipe(gulp.dest('dist'))
     .pipe(cssnano())
@@ -37,16 +22,26 @@ gulp.task('dist:css', function () {
 })
 
 gulp.task('dist:js', function () {
-  return gulp.src('app/*.html').pipe(useref()).pipe(rename('app.js')).pipe(gulp.dest('dist'))
+  return gulp.src('./app/*.html').pipe(useref()).pipe(rename('./app.js')).pipe(gulp.dest('dist'))
 })
 
-gulp.task('watch', gulp.series(['browserSync', 'sass']), function () {
-  gulp.watch('app/scss/**/*.scss', gulp.series(['sass']))
-  gulp.watch('app/examples/*.html', browserSync.reload)
-  gulp.watch('app/js/**/*.js', browserSync.reload)
-})
+gulp.task(
+  'watch',
+  gulp.series('sass', function () {
+    browserSync.init({
+      server: {
+        baseDir: './app'
+      },
+      browser: 'default',
+      injectChanges: true,
+      notify: false
+    })
+    gulp.watch('./app/scss/**/*.scss', gulp.series('sass'))
+    gulp.watch('./app/*.html', browserSync.reload)
+  })
+)
 
-/* Build the app */
-gulp.task('build', gulp.series(['dist:css', 'dist:js']), function () {
+/* Build the ./app */
+gulp.task('build', gulp.series('dist:css', 'dist:js'), function () {
   console.log('Building Colored Pages')
 })
